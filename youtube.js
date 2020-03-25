4f2af480755dd3e98640d8e7f6cac14d327b02ab
Chronophylos <nikolai@chronophylos.com>
2020-03-26 00:42:44 +0100

    formatting, switch and comments

--- youtube.js
+++ youtube.js
@@ -1,58 +1,75 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// This code loads the IFrame Player API code asynchronously
+var tag = document.createElement('script')
+tag.src = 'https://www.youtube.com/player_api'
+var firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-var setVideoURLButton = document.getElementById('set-video-url');
+var setVideoURLButton = document.getElementById('set-video-url')
 
-var player;
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('youtube-player', {
-        height: '607',
-        width: '1080',
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-function onPlayerStateChange(event) {
-    var playerStatus = event.data;
-    var text;
-    if (playerStatus == -1) {
-        text = "unstarted";
-    } else if (playerStatus == 0) {
-        text = "ended";
-    } else if (playerStatus == 1) {
-        text = "playing";
-    } else if (playerStatus == 2) {
-        text = "paused";
-    } else if (playerStatus == 3) {
-        text = "buffering";
-    } else if (playerStatus == 5) {
-        text = "video cued";
+// This function creates an <iframe> (and YouTube player) after the API code
+// downloads
+var player
+function onYouTubePlayerAPIReady () {
+  player = new YT.Player('youtube-player', {
+    height: '607',
+    width: '1080',
+    events: {
+      'onStateChange': onPlayerStateChange
     }
-    if (text) {
-        var url = player.getVideoUrl();
-        document.getElementById('player-id').innerHTML = "<a href=" + url + ">" + url + "</a>";
-        document.getElementById('player-state').innerHTML = text;
-        document.getElementById('player-pos').innerHTML = player.getCurrentTime() + "s";
-    }
-}
-function setVideoId() {
-    if (!player) {return;}
-    var newId = document.getElementById('video-id').value;
-    player.cueVideoById({
-        videoId: newId
-    });
+  })
 }
 
-function playVideo() {
-    player.playVideo();
+// The API calls this function when the player's state changes. The function
+// indicates that when playing a video (state=1), the player should play for
+// six seconds and then stop.
+function onPlayerStateChange (event) {
+  let text
+  switch (event.data) {
+    case -1:
+      text = 'unstarted'
+      break
+    case 0:
+      text = 'ended'
+      break
+    case 1:
+    // notify other clients and pause if not everyone is ready
+      text = 'playing'
+      break
+    case 2:
+    // notify other clients if not pausing because one client is still buffering
+      text = 'paused'
+      break
+    case 3:
+      // notify other clients of new position
+      text = 'buffering'
+      break
+    case 5:
+      text = 'video cued'
+      break
+  }
+
+  if (text) {
+    let url = player.getVideoUrl()
+    document.getElementById('player-id').innerHTML = '<a href=' + url + '>' + url + '</a>'
+    document.getElementById('player-state').innerHTML = text
+    document.getElementById('player-pos').innerHTML = player.getCurrentTime() + 's'
+  }
 }
 
-function pauseVideo() {
-    player.pauseVideo();
+function setVideoId () {
+  if (!player) { return }
+  let newId = document.getElementById('video-id').value
+  player.cueVideoById({
+    videoId: newId
+  })
+}
+
+function playVideo () {
+  player.playVideo()
+}
+
+function pauseVideo () {
+  player.pauseVideo()
 }
 
 // https://andybrewer.github.io/mvp/quickstart.html.txt
